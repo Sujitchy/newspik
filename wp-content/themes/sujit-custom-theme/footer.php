50071475f3d8b31c22f33a11dcc5f685c1e0a4e9
Sujit Chy <sujichy919@gmail.com>
2025-07-30 16:33:47 +0545

    uddate footer and css

--- wp-content/themes/sujit-custom-theme/footer.php
+++ wp-content/themes/sujit-custom-theme/footer.php
@@ -3,22 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>footer</title>
+    <title>Footer</title>
+     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/footer.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
 </head>
 <body>
-
-<footer>
-    <div class="footer-menu">
-        <?php
-            wp_nav_menu(array(
-                'theme_location' => 'footer_menu',
-                'menu_class' => 'footer-menu-list'
-            ));
-        ?>
+    <footer class="site-footer">
+  <div class="footer-top">
+    <div class="footer-logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="NEWSPIK Logo">
+      <p> NEWSPIK  <br>
+        <h6></h6>
+      </p>
     </div>
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. सबै अधिकार सुरक्षित।</p>
-    <?php wp_footer(); ?>
-    Powered by <a href="https://mohrain.com">Mohrain Websoft</a>
-</footer>
+
+    <div class="footer-contact">
+      <h3>Contact Us</h3>
+      <ul>
+        <li><i class="fas fa-map-marker-alt"></i> Nepal</li>
+        <li><i class="fas fa-phone"></i> +977-1-00000000</li>
+        <li><i class="fas fa-envelope"></i> newspik@gmail.com</li>
+      </ul>
+    </div>
+
+    <div class="footer-links">
+      <h1>हाम्रो टिम</h1>
+      <ul>
+        <li><i class="fas fa-angle-double-right"></li>
+        <li><i class="fas fa-angle-double-right"></i></li>
+      </ul>
+    </div>
+  </div>
+    </footer>
+  <div class="footer-bar">
+    © 2025 NEWSPIK Pvt. Ltd. | All Rights Reserved. | Design & Developed By Mohrain
+  </div>
 </body>
 </html>
